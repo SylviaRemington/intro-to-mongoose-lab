7a8652477e3e161956b9203133c604d5d9330ff9
SylviaRemington <sylviaremington@gmail.com>
2025-06-03 21:29:10 -0700

    adding prompt js code and testing with node

--- app.js
+++ app.js
@@ -4,3 +4,20 @@
 // to create a terminal-based CRM application that will have full CRUD functionality 
 // on a single model: Customer.
 
+// Adding/Installing a dependency/package called prompt-sync
+// Prompt-sync is a package that allows us to easily collect input from a user in the terminal. 
+// This is how we will interact with our application. (npm i prompt-sync)
+
+// To ensure prompt-sync is working, I added the code below. 
+// But I also tested it using node by typing the following in the terminal:
+// node app.js --The terminal then prompted me: "What is your name?" 
+// --I responded: "Sylvia" 
+// --The terminal responded: "Your name is Sylvia"
+// Thus, WORKING! Yay.
+
+
+const prompt = require('prompt-sync')();
+
+const username = prompt('What is your name? ');
+
+console.log(`Your name is ${username}`);
