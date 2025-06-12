0c44f3ebaa544e55a0ae7e0eaf881e60c45df310
SylviaRemington <sylviaremington@gmail.com>
2025-06-12 00:34:50 -0700

    addtl notes regarding schemas and models

--- models/customer.js
+++ models/customer.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 
 // Creating a new model file with a schema that includes name (String) and age (Number).
 // Creating the Schema (aka defining the schema. This is the first step before creating the model.)
+// Schema is the structure that we create for the non relational database.
 const customerSchema = new mongoose.Schema ({
     name: String,
     age: Number,
 })
 
-// Compiling the schema into a model (2nd step after creating schema)
+// Compiling the schema into a model (2nd step after creating schema) // A model talks to the database.
 const Customer = mongoose.model('Customer', customerSchema);
 
 // Exporting the model
