7248484c86fa47aa693b9fa72e2e07e79440e5b6
SylviaRemington <sylviaremington@gmail.com>
2025-06-11 21:32:15 -0700

    addtl notes explaining what schema and model does

--- models/customer.js
+++ models/customer.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 // Creating a new model file with a schema that includes name (String) and age (Number).
-// Creating the Schema
+// Creating the Schema (aka defining the schema. This is the first step before creating the model.)
 const customerSchema = new mongoose.Schema ({
     name: String,
     age: Number,
 })
 
-// Compiling the schema into a model
+// Compiling the schema into a model (2nd step after creating schema)
 const Customer = mongoose.model('Customer', customerSchema);
 
-// Export the model
+// Exporting the model
 module.exports = Customer;
